46b22b933c4ff59efa821856137ef966158e3d90
li-cindy <li.cindy98@gmail.com>
2015-11-10 22:57:46 -0500

    quiz page shoes top 20 top rated movies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, Request
 import urllib2, json
 
 app = Flask(__name__)
@@ -9,7 +9,30 @@ def index():
     
 @app.route("/quiz")
 def quiz():
-    return render_template("quiz.html")
+    key = "bb8c8b5dede831baad7b87391d01d20c"
+    uri = "https://api.themoviedb.org/3/movie/top_rated?api_key=%s"
+    url = uri%(key)
+    
+    request = urllib2.urlopen(url)
+    result = request.read()
+    r = json.loads(result)
+
+    topRated = []
+    for item in r['results']:
+        topRated.append(item['title'])
+
+    #solely for aesthetic purposes to separate into two columbs
+    firstHalf = []
+    secondHalf = []
+    count = 0
+    for item in topRated:
+        if count < 10:
+            firstHalf.append(topRated[count])
+        else:
+            secondHalf.append(topRated[count])
+        count += 1
+
+    return render_template("quiz.html", firstTopMovies = firstHalf, secondTopMovies = secondHalf)
 
 #@app.route("/quiz/<movie>")
 
