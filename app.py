809ed7914061c156da863641312231711857346e
li-cindy <li.cindy98@gmail.com>
2015-11-10 21:00:37 -0500

    css works

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import urllib2, json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hello"
+    return render_template("index.html")
     
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
+
+#@app.route("/quiz/<movie>")
+
+if __name__ == "__main__":
+   app.debug = True
+   app.run(host="0.0.0.0", port=8000)
