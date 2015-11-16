f61a071845cc8875cbc37a59671fbe56c1609fe1
li-cindy <li.cindy98@gmail.com>
2015-11-16 16:16:42 -0500

    give up works

--- app.py
+++ app.py
@@ -20,14 +20,31 @@ def submission(tag):
     if request.method == "GET": 
         return appextended.specMovie(tag)
     else:
+        key = "bb8c8b5dede831baad7b87391d01d20c"
+        creds = "append_to_response=credits"
+        uri = "https://api.themoviedb.org/3/movie/%s?api_key=%s&%s"
+        url = uri%(tag, key, creds)
+        
+        requested = urllib2.urlopen(url)
+        result = requested.read()
+        r = json.loads(result)
+        
+        cast = r["credits"]["cast"]
+        
+        actor = []
+        character = []
+        for i in range (0, 5):
+            actor.append(cast[i]["name"])
+            character.append(cast[i]["character"])
+
         button = request.form['button']
         if button == "Submit":
             status = "submitted"
-            return render_template("results.html", status = status)
+            return render_template("results.html", status = status, character=character, actor=actor)
         else:
             status = "giveup"
-            return render_template("results.html", status = status)
-
+            return render_template("results.html", status = status, character=character, actor=actor)
+            
 
 if __name__ == "__main__":
    app.debug = True
